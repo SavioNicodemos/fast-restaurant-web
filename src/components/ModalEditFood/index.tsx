04db867e413b7c0f78e7c5a0c17d4b26361288a4
SavioNicodemos <nicodemosgcosta@gmail.com>
2024-01-24 23:14:34 +0000

    Lint files

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -44,12 +44,16 @@ const ModalEditFood = ({ isOpen, setIsOpen, editingFood, handleUpdateFood }: IMo
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
       <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood} placeholder=''>
         <h1>Editar Prato</h1>
-        <Input name='image' data-testid="edit-food-form-image" placeholder='Cole o link aqui' />
+        <Input name='image' data-testid='edit-food-form-image' placeholder='Cole o link aqui' />
 
-        <Input name='name' data-testid="edit-food-form-name" placeholder='Ex: Moda Italiana' />
-        <Input name='price' data-testid="edit-food-form-price" placeholder='Ex: 19.90' />
+        <Input name='name' data-testid='edit-food-form-name' placeholder='Ex: Moda Italiana' />
+        <Input name='price' data-testid='edit-food-form-price' placeholder='Ex: 19.90' />
 
-        <Input name='description' data-testid="edit-food-form-description" placeholder='Descrição' />
+        <Input
+          name='description'
+          data-testid='edit-food-form-description'
+          placeholder='Descrição'
+        />
 
         <button type='submit' data-testid='edit-food-button'>
           <div className='text'>Editar Prato</div>
